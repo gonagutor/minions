0a73f5389ffe50e3e2e3a6fea3e68f5754b2f9a1
Gonzalo Aguado Torres <gonagutor@gmail.com>
2021-03-22 04:58:04 +0100

    Initial commit

---
+++ src/main/java/com/gonagutor/minions/Minions.java
@@ -0,0 +1,17 @@
+package com.gonagutor.minions;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Minions extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
